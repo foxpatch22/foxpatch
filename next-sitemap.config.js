597ec9c88c2efafd311165c9352545ec28ad0360
Alpha-trainer <thealpha06667@gmail.com>
2025-08-09 19:20:22 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://foxpatch.vercel.app',
-  generateRobotsTxt: true,
-  outDir: 'public',
-  exclude: ['/api/*'],
+  siteUrl: process.env.SITE_URL || 'https://foxpatch.vercel.app',
+  generateRobotsTxt: true
 };
